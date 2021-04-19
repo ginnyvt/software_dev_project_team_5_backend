64f09cd19683060262b3f5f0e5e728490c8c80f0
IbraheemLanre <s2001108@edu.bc.fi>
2021-04-19 15:43:40 +0300

    uninstalled body-parser

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -14,7 +13,7 @@ const roleRoutes = require('./routes/roles');
 const jobRoutes = require('./routes/jobs');
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/users', userRoutes);
 app.use('/roles', roleRoutes);
