adb212496a38374a1b92c1970943426a4d85adfc
asus-ubuntu <ginny@powerkernel.com>
2021-05-07 02:03:39 +0300

    chore: turn off cors

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const mongoose = require('mongoose');
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
-const corsOption = {
-  origin: process.env.CLIENT_ORIGIN_URL // remember to add the client origin url
-}
+// remember to add the client origin url
+// const corsOption = {
+//   origin: process.env.CLIENT_ORIGIN_URL
+// }
 const app = express();
 
 // Import routers
@@ -16,17 +17,15 @@ const userRoutes = require('./routes/users');
 const roleRoutes = require('./routes/roles');
 const jobRoutes = require('./routes/jobs');
 
-
 app.use(cors());
 app.use(express.json());
-
 
 app.use('/users', userRoutes);
 app.use('/roles', roleRoutes);
 app.use('/jobs', jobRoutes);
 
 const connection_url = process.env.MONGO_URL;
-console.log(connection_url);
+// console.log(connection_url);
 
 mongoose
   .connect(connection_url, { useNewUrlParser: true, useUnifiedTopology: true })
