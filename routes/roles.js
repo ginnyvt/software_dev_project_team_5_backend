628b9f3817e1553a7e8c8c7f199c9ee70acf17b5
asus-ubuntu <ginny@powerkernel.com>
2021-05-16 23:54:08 +0300

    chore: remove checkRoles.js

--- routes/roles.js
+++ routes/roles.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { checkJwt } = require('../helpers/check-jwt');
-const { checkRoles } = require('../helpers/checkRoles');
 const { getAccessToken } = require('../helpers/accessToken');
 const axios = require('axios');
 
